1ea08ed7b409ebd2a995467c922405fa961c3a3a
Lozxofficial <lozx851@gmail.com>
2024-12-05 17:33:57 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,70 @@
+input.onButtonPressed(Button.A, function () {
+    if (page_number > 0) {
+    	
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    page_number = 1
+})
+input.onButtonPressed(Button.B, function () {
+    if (page_number == 1) {
+    	
+    }
+    if (page_number == 2) {
+    	
+    }
+    if (page_number == 3) {
+    	
+    }
+    if (page_number == 4) {
+    	
+    }
+    if (page_number == 5) {
+    	
+    }
+    if (page_number == 6) {
+    	
+    }
+})
+let page_number = 0
+let strip = neopixel.create(DigitalPin.P0, 90, NeoPixelMode.RGB_RGB)
+strip.setBrightness(30)
+strip.showRainbow(1, 360)
 basic.forever(function () {
-	
+    if (page_number > 0) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (page_number == 7) {
+        page_number = 1
+    }
+    if (page_number == 1) {
+        basic.showNumber(1)
+    }
+    if (page_number == 2) {
+        basic.showNumber(2)
+    }
+    if (page_number == 3) {
+        basic.showNumber(3)
+    }
+    if (page_number == 4) {
+        basic.showNumber(4)
+    }
+    if (page_number == 5) {
+        basic.showNumber(5)
+    }
+    if (page_number == 6) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # .
+            # . . . #
+            . # # # .
+            `)
+    }
 })
